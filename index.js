8a7a0eb2b35911bb596369624878d39216e10583
remiconnesson <remiconnesson2@gmail.com>
2019-08-28 10:22:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const webcamElement = document.getElementById('webcam');
 
 async function setupWebcam() {
-    return new Promise((resolve, reject) => {
-      const navigatorAny = navigator;
-      navigator.getUserMedia = navigator.mediaDevices.getUserMedia || 
-          navigator.getUserMedia || navigatorAny.webkitGetUserMedia || 
-          navigatorAny.mozGetUserMedia ||
-          navigatorAny.msGetUserMedia;
-      if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true},
-          stream => {
-            webcamElement.srcObject = stream;
-            webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-          },
-          error => reject());
-      } else {
-        reject();
-      }
-    });
-  }
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
 
 let net;
 
